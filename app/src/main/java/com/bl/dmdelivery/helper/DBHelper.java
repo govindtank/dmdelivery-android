a384fcb3c5127ea1201c2afcb3eff378a60fec4c
iCoding2 <34499758+iCoding2@users.noreply.github.com>
2018-03-20 12:15:16 +0700

    update 20-03-2018 12:15 แก้ไข getUnpackListWithmultiInv
    update 20-03-2018 12:15 แก้ไข getUnpackListWithmultiInv

--- app/src/main/java/com/bl/dmdelivery/helper/DBHelper.java
+++ app/src/main/java/com/bl/dmdelivery/helper/DBHelper.java
@@ -864,8 +864,11 @@ public class DBHelper extends SQLiteOpenHelper {
 //            cursor = sqLiteDatabase.rawQuery(" SELECT transno,unpack_code,unpack_desc,SUM(unpack_qty) AS unpack_qty,unpack_image,rep_name FROM " + TableUnpack + " WHERE transno IN (" + sigGetData + ")" ,null);
 
 
-            cursor = sqLiteDatabase.rawQuery(" SELECT unpack_code,unpack_desc, (SELECT  SUM(unpack_qty) FROM "  + TableUnpack  + " WHERE transno IN (" + sigGetData + ") GROUP BY unpack_code) unpack_qty FROM " + TableUnpack + " WHERE transno IN (" + sigGetData + ") GROUP BY unpack_code,unpack_desc" ,null);
+//            cursor = sqLiteDatabase.rawQuery(" SELECT unpack_code,unpack_desc, (SELECT  SUM(unpack_qty) FROM "  + TableUnpack  + " WHERE transno IN (" + sigGetData + ") GROUP BY unpack_code) unpack_qty FROM " + TableUnpack + " WHERE transno IN (" + sigGetData + ") GROUP BY unpack_code,unpack_desc,unpack_qty" ,null);
 
+
+
+            cursor = sqLiteDatabase.rawQuery(" SELECT unpack_code,unpack_desc, SUM(unpack_qty) AS unpack_qty FROM " + TableUnpack + " HAVING BY transno IN (" + sigGetData + ") GROUP BY unpack_code,unpack_desc, SUM(unpack_qty)" ,null);
             if (cursor != null  && cursor.getCount()>0) {
                 cursor.moveToFirst();
             }
